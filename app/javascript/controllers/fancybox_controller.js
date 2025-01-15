b1c08469f67d96e45ff62e4fe577c69030188164
Louis Le Doussal <louis.le-doussal@mmibordeaux.com>
2025-01-15 14:34:18 +0100

    feat: ajoute du front un peu partout

---
+++ app/javascript/controllers/fancybox_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    console.log("fancybox connecté");
+    this.initFancybox();
+  }
+
+  initFancybox() {
+    if (typeof $.fancybox === 'function') {
+      $("[data-fancybox]").fancybox({
+        loop: true,
+        buttons: [
+          "close"
+        ],
+      });
+    } else {
+      console.error("FancyBox n'est pas chargé correctement");
+    }
+  }
+
+}
