ce1398fab674ac507d011980a4bdd1d065b9cb44
Nat-intosh <nvandaele1234@gmail.com>
2025-01-18 18:40:19 +0100

    feat: host config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,8 @@ Rails.application.configure do
 
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
-
+  config.hosts << "musemoji.natvand.eu.org"
+  config.force_ssl = true
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
